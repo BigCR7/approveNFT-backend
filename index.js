a0cd579048fc0cc6e4c46aeba7b98203436df368
BigCR7 <sergiosantos9966@gmail.com>
2022-07-16 02:09:06 -0500

    fix fetching tokenIDs

--- index.js
+++ index.js
@@ -5,33 +5,40 @@ import fetch from "node-fetch";
 
 const router = express.Router();
 
-var app = express();  
+var app = express();
 app.use("/", router);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
 app.post('/nfts', (req, res) => {
-    const account = req.body.account;    
-    get_opensea_data(account).then((result) => res.send({nftInfo: result}))  
+    const account = req.body.account;
+    get_opensea_data(account).then((result) => res.send({ nftInfo: result }))
 })
 
-var server = app.listen(8000, function () {  
-  var port = server.address().port;  
-  console.log('App listening at %s', port);  
-});  
+var server = app.listen(8000, function () {
+    var port = server.address().port;
+    console.log('App listening at %s', port);
+});
 
 async function get_opensea_data(address) {
-    const response1 = await fetch(`https://api.opensea.io/api/v1/assets?owner=${address}`, {method: 'GET'}) // for testnet: testnets-api.opensea.io
-    const tokenIdInfo = (await response1.json()).assets
-
+    const baseURL = "https://eth-mainnet.g.alchemy.com/v2/FCWZFXUmrFtpAlUGlzQsOBdJfQaZZCzt"; // for mainnet
+    // const baseURL = "https://eth-rinkeby.alchemyapi.io/v2/ohNNPm68LXUZDHC_MPNS9f_LCXBPcI5F"; // for rinkeby
+    const url = `${baseURL}/getNFTs/?owner=${address}`;
+    var requestOptions = {
+        method: 'get',
+        redirect: 'follow'
+    };
+    const response1 = await fetch(url, requestOptions)
+    const tokenIdInfo = (await response1.json()).ownedNfts
+ 
     const response2 = await fetch(`https://api.opensea.io/api/v1/collections?offset=0&asset_owner=${address}`)
     const json = await response2.json()
 
     var tokens_grouped_by_token_address_and_price;
     var tokens_grouped_by_token_address = [];
 
-    json.reduce((acc, nft) => {
+    json?.reduce((acc, nft) => {
         if (nft.primary_asset_contracts.length ===  0){
             tokens_grouped_by_token_address_and_price = acc;
             return acc
@@ -41,10 +48,10 @@ async function get_opensea_data(address) {
         }
         const tokenAddress = nft.primary_asset_contracts[0].address.toLowerCase();
         acc[tokenAddress] = {"price": nft.stats.seven_day_average_price, "schema": nft.primary_asset_contracts[0].schema_name}
-        
-        const result = tokenIdInfo.filter((item) => item.asset_contract.address === tokenAddress)
+
+        const result = tokenIdInfo.filter((item) => item.contract.address === tokenAddress)
         let tokenIds = [];
-        result.map((item) => tokenIds.push(item.token_id))
+        result.map((item) => tokenIds.push(parseInt(item.id.tokenId)))
         tokens_grouped_by_token_address[tokenAddress] = tokenIds
         tokens_grouped_by_token_address_and_price = acc;
         return acc
@@ -64,6 +71,6 @@ async function get_opensea_data(address) {
     nfts.sort((a, b) => {
         return b.price - a.price
     })
-    
+
     return nfts;
 }
